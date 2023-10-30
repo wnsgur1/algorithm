694805f14c663b5b553e5c6a56af9c149885c05e
wnsgur1 <133772349+wnsgur1@users.noreply.github.com>
2023-10-30 10:10:49 +0900

    clear 10093

--- 10093.py
+++ 10093.py
@@ -1,4 +1,16 @@
 a,b=map(int,input().split())
-print(b-a-1)
-for i in range(a+1,b):
-    print(i,end=" ")
+list = []
+if a>b:
+    print(a-b-1)
+    for i in range(b+1,a):
+        list.append(i)
+elif b>a:
+    print(b-a-1)
+    for i in range(a+1,b):
+        list.append(i)
+else:
+    print(0)
+
+
+for i in list:
+    print(i, end=' ')
