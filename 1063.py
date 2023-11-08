3f5f22372eeb872664c6309cbe6f6b1e629c7eb2
wnsgur1 <133772349+wnsgur1@users.noreply.github.com>
2023-11-08 19:26:19 +0900

    failing 1063

--- 1063.py
+++ 1063.py
@@ -1,1 +1,23 @@
-print("미안해 빨리 할게 진짜 미안해")
+a = list(input().split())
+pan = [[0 for i in range(8)]for i in range(8)]
+list = []
+
+print(a[0])
+print(a[0][0], a[0][1])
+for i in range(ord("A"), ord("I")):
+    if a[0][0] == chr(i):
+        a[0].remove(0)
+        print(a[0])
+
+
+# for i in range(int(c)):
+#     move = input()
+#     for j in move:
+#         if j == "R":
+#             print("R")
+#         if j == "L":
+#             print("L")
+#         if j == "T":
+#             print("T")
+#         if j == "B":
+#             print("B")
