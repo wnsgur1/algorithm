3a6324723d6b0f574d85f8340b83c862e3af025a
wnsgur1 <133772349+wnsgur1@users.noreply.github.com>
2023-11-09 23:23:39 +0900

    failing 13116

--- 13116.py
+++ 13116.py
@@ -5,13 +5,14 @@ for i in range(n):
     a, b = map(int,input().split())
     c = 0
     while(a > 0 and b > 0):
-        a = a//2
-        b = b//2
         aList.append(a)
         bList.append(b)
+        a = a//2
+        b = b//2
+
     for j in aList:
         for k in bList:
             if j == k and j > c:
                 c = j
     print(c*10)
-                
+            
