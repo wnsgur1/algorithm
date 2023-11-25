757f1a05eaddaedd06dd097a9b153da80b2e2dd1
wnsgur1 <133772349+wnsgur1@users.noreply.github.com>
2023-11-25 22:04:35 +0900

    clear 1068

--- 1083.py
+++ 1083.py
@@ -1,15 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+def dfs(num, arr):
+    arr[num] = -2
+    for i in range(len(arr)):
+        if num == arr[i]:
+            dfs(i, arr)
+
 n = int(input())
-array = list(map(int,input().split()))
-c = int(input())
+arr = list(map(int, input().split()))
+k = int(input())
+count = 0
 
-
-for i in range(0, n, 2):
-    array[i], array[i+1] = array[i+1], array[i]
-    c-=1
-    if (c==0):
-        break
-
-
-
-for i in array:
-    print(i, end=" ")
+dfs(k, arr)
+count = 0
+for i in range(len(arr)):
+    if arr[i] != -2 and i not in arr:
+        count += 1
+print(count)
