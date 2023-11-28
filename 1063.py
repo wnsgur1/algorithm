49eb023bc0f48cacfcd51025411ee2070d17ac17
wnsgur1 <133772349+wnsgur1@users.noreply.github.com>
2023-11-28 17:25:44 +0900

    clear 1063

--- 1063.py
+++ 1063.py
@@ -1,23 +1,20 @@
-a = list(input().split())
-pan = [[0 for i in range(8)]for i in range(8)]
-list = []
+king, stone, N = input().split()
+k = list(map(int, [ord(king[0]) - 64, king[1]]))
+s = list(map(int, [ord(stone[0]) - 64, stone[1]]))
+move = {'R': [1, 0], 'L': [-1, 0], 'B': [0, -1], 'T': [0, 1], 'RT': [1, 1], 'LT': [-1, 1], 'RB': [1, -1], 'LB': [-1, -1]}
 
-print(a[0])
-print(a[0][0], a[0][1])
-for i in range(ord("A"), ord("I")):
-    if a[0][0] == chr(i):
-        a[0].remove(0)
-        print(a[0])
-
-
-# for i in range(int(c)):
-#     move = input()
-#     for j in move:
-#         if j == "R":
-#             print("R")
-#         if j == "L":
-#             print("L")
-#         if j == "T":
-#             print("T")
-#         if j == "B":
-#             print("B")
+for _ in range(int(N)):
+    m = input()
+    nx = k[0] + move[m][0]
+    ny = k[1] + move[m][1]
+    if 0 < nx <= 8 and 0 < ny <= 8:
+        if nx == s[0] and ny == s[1]:
+            sx = s[0] + move[m][0]
+            sy = s[1] + move[m][1]
+            if 0 < sx <= 8 and 0 < sy <= 8:
+                k = [nx, ny]
+                s = [sx, sy]
+        else:
+            k = [nx, ny]
+print(f'{chr(k[0] + 64)}{k[1]}')
+print(f'{chr(s[0] + 64)}{s[1]}')
