8806c76c15867e4b03776a8e49b4e0b667e26c68
wnsgur1 <133772349+wnsgur1@users.noreply.github.com>
2023-11-30 21:20:10 +0900

    clear 1072

--- 1072.py
+++ 1072.py
@@ -1,9 +1,23 @@
-a,b = map(int,input().split())
-z1 = b/a*100
-z2 = z1
-cnt= 0
-while(int(z1) == int(z2)):
-    cnt+=1
-    b+=1
-    z1 = b/a*100
-print(cnt)
+
+import sys
+input = sys.stdin.readline
+ 
+ 
+X, Y = map(int, input().split())
+Z = (Y * 100) // X
+if Z >= 99:
+    print(-1)
+else:
+    answer = 0
+    left = 1
+    right = X
+ 
+    while left <= right:
+        mid = (left + right) // 2
+        if (Y+mid)*100 // (X+mid) <= Z:
+            left = mid+1
+        else:
+            answer = mid
+            right = mid - 1
+ 
+    print(answer)
