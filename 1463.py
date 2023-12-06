d54087ff4233d0bab6b66a2eabc167f9cac70a88
wnsgur1 <133772349+wnsgur1@users.noreply.github.com>
2023-12-06 21:52:07 +0900

    failing 1463

--- 1463.py
+++ 1463.py
@@ -1,68 +1,14 @@
-def first(a):
-    cnt = 0
-    if a%3 == 0:
-        a = a//3
-        cnt+=1
+n = int(input())
 
-    while (a != 1):
-        if a%3 == 0:
-            a = a//3
-        elif a%2 == 0:
-            a = a//2
-        else:
-            a-=1
-        cnt+=1
+list1 = [0]*n
 
-
-    return cnt
-
-def second(a):
-    cnt = 0
-    if a%2 == 0:
-        a = a//2
-        cnt+=1
-
-    while (a!= 1):
-        if a%3 == 0:
-            a = a//3
-        elif a%2 == 0:
-            a = a//2
-        else:
-            a-=1
-        cnt+=1
-
-
-
-    return cnt
-
-def third(a):
-    cnt = 1
-    a-=1
-
-    while (a!= 1):
-        if a%3 == 0:
-            a = a//3
-        elif a%2 == 0:
-            a = a//2
-        else:
-            a-=1
-        cnt+=1
-
-
-
-    return cnt
-
-def main():
-    
-    x = int(input())
-    if(x == 1):
-        print(x)
-    else:
-        a = first(x)
-        b = second(x)
-        c = third(x)
-
-        print((a if a<b else b) if ((b if b<a else a)<c) else c)
-
-
-main()
+for i in range(1, n+1):
+    list2 = []
+    list2.append(list1[i-1]+1)
+    if i%2 == 0:
+        list2.append(list1[i//2]+1)
+    if i%3 == 0:
+        list2.append(list1[i//3]+1)
+    list1.pop(i)
+    list1.insert(i, min(list2))
+    print(list1)
