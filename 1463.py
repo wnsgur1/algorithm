cab54c00a4b5a41b7933a242be5ef5b94821e27d
wnsgur1 <133772349+wnsgur1@users.noreply.github.com>
2023-12-09 23:24:58 +0900

    failing 1463

--- 1463.py
+++ 1463.py
@@ -1,15 +1,20 @@
 n = int(input())
 
-list1 = [0]*n
+list1 = [0 for i in range(n+1)]
 
-for i in range(n):
+for i in range(1, n):
     list2 = []
     list2.append(list1[i-1]+1)
+
     if i%2 == 0:
         list2.append(list1[i//2]+1)
+
     if i%3 == 0:
         list2.append(list1[i//3]+1)
+        
     list1.pop(i)
     list1.insert(i, min(list2))
-    
-print(list1[-1])
+    print(list2)
+
+
+print(list1[-2]+1)
