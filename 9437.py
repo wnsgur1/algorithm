30ac79915dcfdc1566cc7c91f103f2e95f3db688
wnsgur1 <133772349+wnsgur1@users.noreply.github.com>
2024-02-15 15:09:44 +0900

    clear 9437

--- 9437.py
+++ 9437.py
@@ -8,5 +8,4 @@ while(True):
             Page = [2*i+1, 2*i+2, t[0]-2*i-1, t[0]-2*i]
             if t[1] in Page:
                 Page.remove(t[1])
-                print(*Page)  
-                  
+                print(*Page)
