2046db878b8bc87fdb09027066143022fa0c74cd
wnsgur1 <133772349+wnsgur1@users.noreply.github.com>
2024-02-27 22:03:46 +0900

    clear 2083

--- 2083.py
+++ 2083.py
@@ -2,7 +2,9 @@ while(True):
     name, age, kg = input().split()
     if name == '#':
         break
+
     if int(age) > 17 or int(kg) >= 80:
         print(name, "Senior")
+
     else:
         print(name, "Junior")
