4c79597b6bc96740106c47ba3ae4ae130ffd4225
wnsgur1 <asdf1234@dgsw.hs.kr>
2024-06-30 19:06:03 +0900

    clear 1010

--- 1010.py
+++ 1010.py
@@ -1,4 +1,5 @@
 def factorial(n):
+    
     num = 1
     for i in range(1, n+1):
         num *= i
@@ -7,6 +8,7 @@ def factorial(n):
 T = int(input())
 
 for _ in range(T):
+
     n, m = map(int, input().split())
     bridge = factorial(m) // (factorial(n) * factorial(m - n))
     print(bridge)
