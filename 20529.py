ccd3ba8a9014e6200c061262dd63c04c5887bd68
wnsgur1 <asdf1234@dgsw.hs.kr>
2024-09-25 14:00:26 +0900

    remove file

--- 20529.py
+++
@@ -1,8 +0,0 @@
-a = int(input())
-
-for i in range(a):
-    
-    dis = 0
-    n = int(input())
-    mbti = map(list, input().split())
-    print(mbti)
--- 2083.py
+++
@@ -1,8 +0,0 @@
-while(True):
-    name, age, kg = input().split()
-    if name == '#':
-        break
-    if int(age) > 17 or int(kg) >= 80:
-        print(name, "Senior")
-    else:
-        print(name, "Junior")
