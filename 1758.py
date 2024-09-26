8bd6359ea710d400c53e1dffd9f90d6abf2ea4b3
wnsgur1 <asdf1234@dgsw.hs.kr>
2024-09-26 17:09:46 +0900

    clear 1758

--- 1758.py
+++ 1758.py
@@ -1,3 +1,18 @@
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]
+    lesser_arr, equal_arr, greater_arr = [], [], []
+    for num in arr:
+        if num < pivot:
+            lesser_arr.append(num)
+        elif num > pivot:
+            greater_arr.append(num)
+        else:
+            equal_arr.append(num)
+    return quick_sort(lesser_arr) + equal_arr + quick_sort(greater_arr)
+
+
 def totalTip(line):
     list = line
     listSize = len(list)    
@@ -15,8 +30,12 @@ if __name__ == "__main__":
     for i in range(n):
         line.append(int(input()))
 
-		# 정렬하고 반전
+	# 퀵 정렬
+    # line = quick_sort(line)
+    # line.reverse()
+    # tip = totalTip(line)
+
+    # 파이썬 내장함수 정렬
     line = sorted(line, reverse=True)
     tip = totalTip(line)
-
     print(tip)
