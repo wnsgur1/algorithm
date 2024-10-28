9964df72fe75c5350cdaf36bf7cbb0f99ea66f29
wnsgur1 <asdf1234@dgsw.hs.kr>
2024-10-28 10:59:37 +0900

    clear 10815

--- 10815.py
+++ 10815.py
@@ -1,24 +1,38 @@
 # 상근이가 가진 숫자 카드의 개수 N 입력
 N = int(input())
 
-# 숫자 카드에 적힌 정수를 입력받아 리스트에 저장
-cards = list(map(int, input().split()))
+# 숫자 카드에 적힌 정수를 입력받아 정렬하여 arr1 리스트에 저장
+arr1 = sorted(list(map(int, input().split())))
 
-# 수의 개수 M 입력
+# 확인할 숫자의 개수 M 입력
 M = int(input())
 
-# 상근이가 가지고 있는지 확인할 M개의 정수를 입력받음
-sang = list(map(int, input().split()))
+# 확인할 M개의 정수를 arr2 리스트에 저장
+arr2 = list(map(int, input().split()))
 
-# 결과를 저장할 리스트
-result = []
+# arr2의 각 숫자에 대해 반복
+for number in arr2:
+    s = 0           # 시작 포인터 초기화
+    e = N - 1       # 끝 포인터 초기화
+    flag = False    # 카드의 존재 여부를 표시할 플래그 초기화
 
-# 각 쿼리에 대해 상근이가 숫자 카드를 가지고 있는지 확인
-for query in sang:
-    if query in cards:  # 카드가 있으면 1, 없으면 0
-        result.append(1)
+    # 이진 탐색 수행
+    while s <= e:
+        mid = (s + e) // 2  # 중간 인덱스 계산
+
+        # 중간 값이 찾고자 하는 숫자와 같으면
+        if arr1[mid] == number:
+            flag = True  # 플래그를 True로 설정
+            break  # 반복문 종료
+        # 중간 값이 찾고자 하는 숫자보다 크면
+        elif arr1[mid] > number:
+            e = mid - 1  # 오른쪽 포인터를 중간 값의 왼쪽으로 이동
+        # 중간 값이 찾고자 하는 숫자보다 작으면
+        elif arr1[mid] < number:
+            s = mid + 1  # 왼쪽 포인터를 중간 값의 오른쪽으로 이동
+    
+    # 플래그에 따라 결과 출력
+    if flag:
+        print(1, end=" ")  # 숫자가 카드에 있으면 1 출력
     else:
-        result.append(0)
-
-# 결과 출력
-print(*result)  # 공백으로 구분하여 출력
+        print(0, end=" ")  # 숫자가 카드에 없으면 0 출력
