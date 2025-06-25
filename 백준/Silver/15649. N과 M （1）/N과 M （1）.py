7623066266428b34fb253098ee4ce98f220c2c78
한준혁 <133772349+wnsgur1@users.noreply.github.com>
2025-06-25 20:47:38 +0900

    [Silver III] Title: N과 M (1), Time: 176 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -0,0 +1,17 @@
+def recur(number):
+
+    if number == m:
+        print(*arr)
+        return
+    
+    for i in range(1, n+1):
+        if i in arr:
+            continue
+        arr.append(i)
+        recur(number+1)
+        arr.pop()
+ 
+
+n, m = map(int, input().split())
+arr = []
+recur(0)
