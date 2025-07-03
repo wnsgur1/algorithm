c78f82c2392003548afa25112ba5a01af1c4a904
한준혁 <133772349+wnsgur1@users.noreply.github.com>
2025-07-03 09:19:50 +0900

    [level 3] Title: 네트워크, Time: 0.51 ms, Memory: 9.34 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -0,0 +1,47 @@
+def solution(n, computers):
+    answer = 0
+    v = [False]*n
+    
+    def dfs(node):
+        v[node] = True
+        for i in range(n):
+            if computers[node][i] == 1 and not v[i]:
+                dfs(i)
+    
+    for i in range(n):
+        if not v[i]:
+            dfs(i)
+            answer+=1
+    
+    
+    return answer
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def solution(n, computers):
+#     visited = [False] * n
+
+#     def dfs(node):
+#         visited[node] = True
+#         for i in range(n):
+#             if computers[node][i] == 1 and not visited[i]:
+#                 dfs(i)
+
+#     count = 0
+#     for i in range(n):
+#         if not visited[i]:
+#             dfs(i)
+#             count += 1
+
+#     return count
